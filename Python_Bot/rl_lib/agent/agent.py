2f7db7e830a5a2fa47070492beaaf96385b9ae92
David Szmul <39433222+DavidSzmul@users.noreply.github.com>
2021-09-02 16:35:39 +0200

    Improve Agent + RL library

--- Python_Bot/rl_lib/agent/agent.py
+++ Python_Bot/rl_lib/agent/agent.py
@@ -1,13 +1,20 @@
 import numpy as np
 from abc import ABC, abstractmethod
-import keras
 
 from rl_lib.memory.memory import Experience
 
 class Agent(ABC):
 
     epsilon: float # Ratio between exploration vs exploitation
-    
+    action_shape: np.ndarray
+    state_shape: np.ndarray
+
+    def get_action_shape(self):
+        return self.action_shape
+
+    def get_state_shape(self):
+        return self.state_shape
+
     @abstractmethod
     def get_action(state: np.ndarray) -> np.ndarray:
         '''Determine best action based on state'''
@@ -24,5 +31,23 @@ class Agent(ABC):
     def memorize(experience: Experience) -> float:
         '''Insert new experience into memory'''
 
-    def save_weights(self, filepath, overwrite=False):
-        keras.models.save_model(self.model, filepath, overwrite=overwrite)
+class DefaultAgent(Agent):
+
+    def __init__(self, action_shape: np.ndarray, state_shape: np.ndarray):
+        self.action_shape=action_shape
+        self.state_shape=state_shape
+
+    def get_action(self, state: np.ndarray) -> np.ndarray:
+        return np.any(state!=0)*np.ones(self.action_shape)
+
+    def get_action_training(self, state: np.ndarray) -> np.ndarray:
+        return np.any(state!=0)*np.random.rand(*self.action_shape)
+
+    def fit(self):
+        '''Do nothing'''
+    
+    def memorize(experience: Experience) -> float:
+        '''Do nothing'''
+        return super().memorize()
+        
+
